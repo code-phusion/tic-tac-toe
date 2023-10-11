dfd10fcdf19263c3dc3c0140040b84dae2946942
Igor Skubailo <igor@phokus.io>
2023-10-11 15:10:47 +0300

    Added TODOs

--- src/main/java/com/upskill/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/upskill/tictactoe/TicTacToeApplication.java
@@ -11,3 +11,16 @@ public class TicTacToeApplication {
   }
 
 }
+
+// TODO [igor] Multigame
+
+// TODO Possibility to play with the game
+
+// TODO [board] Restart game (and clear)
+// TODO [board] Display winner (Х or 0);
+// TODO [board] Configurable Win row size (now 3 in a row).
+// TODO [board] Validation: board size >= win row size
+// TODO [board] Validation: min board size 3x3
+// TODO [board] Validation: max board size? (to avoid overload issue)
+// TODO [board] Visualize crossing winner row
+// TODO [board] Replace isDraw -> isGameOver
