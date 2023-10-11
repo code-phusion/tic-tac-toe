cf48fc7e546c9aa141d32065998d169bd0dfd6d6
Taras Anichyn <taras.anichyn@computools.com>
2023-10-11 15:58:00 +0300

    Add more TODO's

--- src/main/java/com/upskill/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/upskill/tictactoe/TicTacToeApplication.java
@@ -24,3 +24,32 @@ public class TicTacToeApplication {
 // TODO [board] Validation: max board size? (to avoid overload issue)
 // TODO [board] Visualize crossing winner row
 // TODO [board] Replace isDraw -> isGameOver
+// TODO [board] Improve UI rendering with big board size
+// TODO [board] Add possibility to choose X or 0 for player
+// TODO [board] Add history of moves with possibility to go back to any move like in chess
+
+
+// TODO [online]
+
+// TODO [online] Add possibility to create room with game [with room_id and password]
+// TODO [online] Add possibility to join room with game [with room_id and password]
+// TODO [online] Add possibility to play with other player [probably we will need to change our API]
+// TODO [online] Add list of available rooms
+
+// TODO [AI]
+
+// TODO [AI] Create AI using algorithms like: MinMax or AlphaBeta [we can discuss what to use]
+// TODO [AI] Add possibility to play with AI
+// TODO [AI] Add possibility to choose AI level
+
+
+// FUTURE-IDEAS:
+// TODO [FUTURE-IDEAS] Implement AI move history (for debugging and analysis)
+// TODO [FUTURE-IDEAS] Implement AI learning capabilities
+// TODO [FUTURE-IDEAS] Optimize AI algorithms for better performance
+// TODO [FUTURE-IDEAS] Implement AI vs AI matches for testing and development purposes
+// TODO [FUTURE-IDEAS] Add AI vs Human mode with adjustable difficulty levels
+// TODO [FUTURE-IDEAS] Create an AI training mode to improve AI skills over time
+// TODO [FUTURE-IDEAS] Implement AI self-improvement mechanisms based on player feedback
+// TODO [FUTURE-IDEAS] Investigate neural network-based AI approaches for more advanced gameplay
+// TODO [FUTURE-IDEAS] Implement tournament mode for AI vs AI matches and for human players
