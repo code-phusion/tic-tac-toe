e47931dde109b2a7d6271becad69367dd0873235
Taras Anichyn <taras.anichyn@computools.com>
2023-11-01 19:53:36 +0200

    add gifs for ending game with AI

--- gui/src/components/TicTacToe.js
+++ gui/src/components/TicTacToe.js
@@ -14,8 +14,9 @@ import { useQuery, useMutation } from '@tanstack/react-query';
 const TicTacToe = () => {
   const { gameId } = useParams();
   const navigate = useNavigate();
-  const [winner, setWinner] = useState(null);
-  const [isDialogOpen, setIsDialogOpen] = useState(false); // New state to control dialog visibility
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [resultImage, setResultImage] = useState(null);
+  const [resultText, setResultText] = useState(null);
 
   const { data: gameState, refetch } = useQuery(
     ['gameState', gameId],
@@ -46,11 +47,10 @@ const TicTacToe = () => {
     if (!gameOver) {
       const currentPlayerSymbol = gameState?.game?.currentPlayerModel?.symbol;
 
-      if (!gameState?.game?.againstAI || (currentPlayerSymbol === 'X')) {
+      if (!gameState?.game?.againstAI || currentPlayerSymbol === 'X') {
         await makeMove({ row, col });
 
-        if (gameState?.game?.againstAI && (currentPlayerSymbol === 'X') && !gameOver) {
-          // Introduce a delay before the AI makes a move to avoid a race condition
+        if (gameState?.game?.againstAI && currentPlayerSymbol === 'X' && !gameOver) {
           setTimeout(async () => {
             await makeAIMove();
           }, 1000);
@@ -82,55 +82,81 @@ const TicTacToe = () => {
 
   const handleClearBoard = () => {
     clearBoard().then(() => {
-      refetch(); // Reload the game state after clearing the board
+      refetch();
     });
   };
 
   useEffect(() => {
     if (gameOver || draw) {
       setIsDialogOpen(true);
+
+      if (gameOver) {
+        const winnerSymbol = gameState?.game?.currentPlayerModel?.symbol;
+        if (gameState?.game?.againstAI) {
+        setResultImage(winnerSymbol === 'O' ? 'https://media.tenor.com/sPGJ7qsV2ukAAAAj/laughing-bender.gif'
+        : 'https://media.tenor.com/dSiQJughhlYAAAAC/fetal-position-futurama.gif');
+        setResultText(`${winnerSymbol} wins!`);
+        } else {
+        setResultText(`${winnerSymbol} wins!`);
+        }
+      } else {
+      if (gameState?.game?.againstAI) {
+        setResultImage('https://media.tenor.com/ihKmG2owX4AAAAAj/what-are-we-gonna-do-bender.gif');
+        setResultText("It's a draw!");
+        }
+        else {
+        setResultText("It's a draw!");
+        }
+      }
     } else {
       setIsDialogOpen(false);
+      setResultImage(null);
+      setResultText(null);
     }
   }, [gameOver, draw]);
 
   return (
-      <div className="game">
-        <Typography variant="h4" gutterBottom>
-          Tic Tac Toe
-        </Typography>
+    <div className="game">
+      <Typography variant="h4" gutterBottom>
+        Tic Tac Toe
+      </Typography>
 
-        <div className="board">
-          {board.map((row, rowIndex) => (
-            <div className="board-row" key={rowIndex}>
-              {row.split('').map((cell, colIndex) => (
-                <div className="cell-container" key={colIndex}>
-                  {renderCell(rowIndex, colIndex, cell)}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-        <Button variant="contained" color="primary" onClick={handleRestart} style={{ margin: '10px' }}>
-          Back to the Main Page
-        </Button>
-        <Button variant="contained" color="secondary" onClick={handleClearBoard} style={{ margin: '10px' }}>
-          Clear Board
-        </Button>
-
-        <Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
-          <DialogTitle>Game Over</DialogTitle>
-          <DialogContent>
-            <Typography>
-              {gameOver ? `${gameState?.game?.currentPlayerModel?.symbol} wins!` : "It's a draw!"}
-            </Typography>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => setIsDialogOpen(false)}>OK</Button>
-          </DialogActions>
-        </Dialog>
+      <div className="board">
+        {board.map((row, rowIndex) => (
+          <div className="board-row" key={rowIndex}>
+            {row.split('').map((cell, colIndex) => (
+              <div className="cell-container" key={colIndex}>
+                {renderCell(rowIndex, colIndex, cell)}
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-    );
+      <Button variant="contained" color="primary" onClick={handleRestart} style={{ margin: '10px' }}>
+        Back to the Main Page
+      </Button>
+      <Button variant="contained" color="secondary" onClick={handleClearBoard} style={{ margin: '10px' }}>
+        Clear Board
+      </Button>
+
+      <Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
+        <DialogTitle>Game Over</DialogTitle>
+        <DialogContent>
+          {resultImage ? (
+            <div>
+              <img src={resultImage} alt="Result" />
+              <Typography>{resultText}</Typography>
+            </div>
+          ) : (
+            <Typography>{resultText}</Typography>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setIsDialogOpen(false)}>OK</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 };
 
 export default TicTacToe;
